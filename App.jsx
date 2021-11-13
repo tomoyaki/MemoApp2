e85f93474c4d74b4fa0a580617f586284c2b17db
tomoyaki <tomoyaki74@gmail.com>
2021-11-13 09:13:34 +0900

    Add all screens to React Navigation

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import TimelineScreen from './src/screens/TimelineScreen';
 import TweetDetailScreen from './src/screens/TweetDetailScreen';
-import TweetModal from './src/screens/TweetModalScreen';
+import TweetModalScreen from './src/screens/TweetModalScreen';
 import AllProfileScreen from './src/screens/AllProfileScreen';
 import ReplyModalScreen from './src/screens/ReplyModalScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
@@ -14,8 +14,13 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={TimelineScreen} />
+      <Stack.Navigator initialRouteName="Timeline">
+        <Stack.Screen name="Timeline" component={TimelineScreen} />
+        <Stack.Screen name="TweetDetail" component={TweetDetailScreen} />
+        <Stack.Screen name="TweetModal" component={TweetModalScreen} />
+        <Stack.Screen name="AllProfile" component={AllProfileScreen} />
+        <Stack.Screen name="ReplyModal" component={ReplyModalScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
