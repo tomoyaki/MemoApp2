0d6726a98d66251515711a35114eef2e827c30d7
tomoyaki <tomoyaki74@gmail.com>
2021-11-13 09:07:48 +0900

    Install React Navigation

--- App.jsx
+++ App.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import TimelineScreen from './src/screens/TimelineScreen';
 import TweetDetailScreen from './src/screens/TweetDetailScreen';
 import TweetModal from './src/screens/TweetModalScreen';
@@ -6,6 +9,16 @@ import AllProfileScreen from './src/screens/AllProfileScreen';
 import ReplyModalScreen from './src/screens/ReplyModalScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
 
-export default function App() {
-  return <AllProfileScreen />;
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={TimelineScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+export default App;
