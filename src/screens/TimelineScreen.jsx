a7b3feffef2f7e48ddddffc73da0fe70338b8f47
tomoyaki <tomoyaki74@gmail.com>
2021-12-26 22:25:15 +0900

    Implement logout button

--- src/screens/TimelineScreen.jsx
+++ src/screens/TimelineScreen.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, ScrollView, StyleSheet } from 'react-native';
 
 import Tweet from '../components/Tweet';
 import CircleButton from '../components/CircleButton';
 import FooterBar from '../components/FooterBar';
+import LogOutButton from '../components/LogOutButton';
 
 export default function TimelineScreen(props) {
   const { navigation } = props;
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <LogOutButton />,
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <ScrollView>
